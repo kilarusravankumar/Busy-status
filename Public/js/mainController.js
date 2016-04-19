12dcb5d5a2c801ebfc6c4c60d6109653875e96c7
sravan kumar kilaru <sravan kumar kilaru>
2016-04-19 17:17:48 -0400

    login and registration page added

--- Public/js/mainController.js
+++ Public/js/mainController.js
@@ -1,12 +1,24 @@
-var app = angular.module('chirpApp', []);
+var app = angular.module('todoApp', []);
 
 app.controller('mainController', function($scope){
   $scope.posts = [];
-  $scope.newPost = {created_by: '', text: '', created_at: ''};
+ emptyPost = {todo: '', text: '', created_at: ''};
 
   $scope.post = function(){
     $scope.newPost.created_at = Date.now();
     $scope.posts.push($scope.newPost);
-    $scope.newPost = {created_by: '', text: '', created_at: ''};
+    $scope.newPost = emptyPost;
   };
 });
+
+app.controller('authController',function($scope){
+    $scope.user={username:'',password:''};
+    $scope.errorMsg='';
+    $scope.register=function(){
+        $scope.errorMsg="your registered as "+ $scope.user.username;
+        $scope.user={username:'',password:''};
+    }
+    $scope.login=function(){
+        $scope.errorMsg="login request for "+$scope.user.username;
+}
+})
